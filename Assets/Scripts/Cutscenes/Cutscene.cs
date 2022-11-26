08e26e80f3f590f1f14d324348d542f057eb65a8
hwelsters <corevalue.noel@gmail.com>
2022-11-26 01:31:16 -0700

    sonks and menu

--- Assets/Scripts/Cutscenes/Cutscene.cs
+++ Assets/Scripts/Cutscenes/Cutscene.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class Cutscene : MonoBehaviour
 {
-    protected List<Action> actions;
+    protected List<Action> actions = new List<Action>();
     protected bool stepComplete = true;
     protected int currentStep = 0;
 
-    public void Start()
+    protected virtual void Start()
     {
         RunAction(actions[0]);
+    }
+
+    protected virtual void Update()
+    {
+        ProgressThroughCutscene();
     }
 
     public bool IsProgressKeyPressed()
     {
         return Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return);
+    }
+
+    public bool IsProgressKey()
+    {
+        return Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Return);
+    }
+
+    public bool IsProgressKeyUp()
+    {
+        return Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.Return);
     }
 
     public void RunAction(Action action)
@@ -27,7 +42,7 @@ public class Cutscene : MonoBehaviour
     public void ProgressThroughCutscene() {
         if (IsProgressKeyPressed() && stepComplete) 
         {
-            currentStep++;
+            currentStep = (int) Mathf.Clamp(currentStep + 1, 0, actions.Count - 1);
             RunAction(actions[currentStep]);
         }
     }
--- Assets/Scripts/Cutscenes/Intro/IntroCutscene.cs
+++ Assets/Scripts/Cutscenes/Intro/IntroCutscene.cs
@@ -2,22 +2,64 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 [Serializable]
 public struct Cut
 {
     public Sprite image;
-    public string text;
+    [TextArea] public string text;
 }
+
 
 public class IntroCutscene : Cutscene
 {
-    [SerializeField]
-    protected List<Cut> scenes;
+    [SerializeField] protected List<Cut> scenes = new List<Cut>();
+    [SerializeField] protected Image introImage;
+    [SerializeField] protected TextMeshProUGUI introText;
+    [SerializeField] protected string nextScene;
 
-    // Start is called before the first frame update
-    void Start() { }
+    private Coroutine textCoroutine;
 
-    // Update is called once per frame
-    void Update() { }
+    protected override void Start()
+    {
+        for(int i = 0; i < scenes.Count; i++) this.actions.Add(() => RunCut());
+        this.actions.Add(()=>ChangeScene());
+        base.Start();
+    }
+
+    private IEnumerator ShowText () {
+        string shownText = "";
+        int index = 0;
+
+        introText.text = "";
+        if (IsProgressKey()) { while (!IsProgressKeyUp()) yield return null; }
+
+        while (index < scenes[currentStep].text.Length && !IsProgressKey())
+        {
+            shownText += this.scenes[currentStep].text[index++];
+            this.introText.text = shownText;
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        introText.text = this.scenes[currentStep].text;
+        while (!IsProgressKeyUp()) yield return null;
+
+        this.stepComplete = true;
+    }
+
+    private void RunCut()
+    {
+        introImage.sprite = scenes[currentStep].image;
+
+        if (textCoroutine != null) StopCoroutine(textCoroutine);
+        textCoroutine = StartCoroutine(ShowText());
+    }
+
+    private void ChangeScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
 }
